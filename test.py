05de4a1c9b5bba51e393ab45ebdaa0985e3ccf68
Vithorio Marques <vithorio.marques@gmail.com>
2023-10-17 15:03:25 -0300

    Using rectangles to place player, snail and snail movement

--- test.py
+++ test.py
@@ -13,12 +13,15 @@ pygame.display.set_caption('Beard Cutter')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font(font_path+'Pixeltype.ttf', 50)
 
-sky_surface = pygame.image.load(graphics_path+'Sky.png')
-ground_surface = pygame.image.load(graphics_path+'ground.png')
+sky_surface = pygame.image.load(graphics_path+'Sky.png').convert_alpha()
+ground_surface = pygame.image.load(graphics_path+'ground.png').convert_alpha()
 text_surface = test_font.render('My game', False, 'Black')
 
-snail_surface = pygame.image.load(graphics_path+'snail/snail1.png')
-snail_x_pos = 0
+snail_surface = pygame.image.load(graphics_path+'snail/snail1.png').convert_alpha()
+snail_rect = snail_surface.get_rect(midbottom = (0,300))
+
+player_surf = pygame.image.load(graphics_path+'Player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80,300))
 
 while True:
     for event in pygame.event.get():
@@ -29,9 +32,14 @@ while True:
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
     screen.blit(text_surface,(300, 50))
-    snail_x_pos -= 3
-    screen.blit(snail_surface,(snail_x_pos,265))
-    if snail_x_pos < -28:
-        snail_x_pos = WIDTH + 12
+
+    snail_rect.x -= 3
+    screen.blit(snail_surface, snail_rect)
+    
+    screen.blit(player_surf,player_rect)
+
+    if snail_rect.x < -28:
+        snail_rect.x = WIDTH + 12
+    
     pygame.display.update()
     clock.tick(60)
