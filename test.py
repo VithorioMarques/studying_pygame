66a6c29fe1658ebef7bddf8f95d3c6b2fc2eb9f1
Vithorio Marques <vithorio.marques@gmail.com>
2023-10-18 21:00:16 -0300

    Working with rectangles for collision, drawing and color/text manipulation

--- test.py
+++ test.py
@@ -15,7 +15,9 @@ test_font = pygame.font.Font(font_path+'Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load(graphics_path+'Sky.png').convert_alpha()
 ground_surface = pygame.image.load(graphics_path+'ground.png').convert_alpha()
-text_surface = test_font.render('My game', False, 'Black')
+
+score_surface = test_font.render('My game', False, (64,64,64))
+score_rect = score_surface.get_rect(center = (WIDTH/2,50))
 
 snail_surface = pygame.image.load(graphics_path+'snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom = (0,300))
@@ -28,10 +30,18 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        # elif event.type == pygame.MOUSEMOTION:
+        #     if player_rect.collidepoint(event.pos):
+        #         print('collision')
 
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
-    screen.blit(text_surface,(300, 50))
+    pygame.draw.rect(screen, '#c0e8ec', score_rect)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect,10)
+
+    #pygame.draw.line(screen, 'Gold', (0,0),(800,400),10)
+
+    screen.blit(score_surface,score_rect)
 
     snail_rect.x -= 3
     screen.blit(snail_surface, snail_rect)
@@ -41,5 +51,12 @@ while True:
     if snail_rect.x < -28:
         snail_rect.x = WIDTH + 12
     
+    # if player_rect.colliderect(snail_rect):
+    #     print('collision')
+
+    # mouse_pos = pygame.mouse.get_pos()
+    # if player_rect.collidepoint((mouse_pos)):
+    #     print('colision')
+
     pygame.display.update()
     clock.tick(60)
