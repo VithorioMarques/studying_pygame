ce63d880352a9896e72c3727a2d736189df19c3a
Vithorio Marques <vithorio.marques@gmail.com>
2023-10-31 13:45:33 -0300

    Adding score

--- test.py
+++ test.py
@@ -6,6 +6,13 @@ HEIGHT = 400
 
 graphics_path = './UltimatePygameIntro/graphics/'
 font_path = './UltimatePygameIntro/font/'
+
+def display_score():
+    current_time = int(pygame.time.get_ticks()/1000 - start_time)
+    score_surf = test_font.render(f'Score: {current_time}', False, (64,64,64))
+    score_rect = score_surf.get_rect(center = (400,50))
+    screen.blit(score_surf, score_rect)
+
 pygame.init()
 
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
@@ -13,12 +20,13 @@ pygame.display.set_caption('Beard Cutter')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font(font_path+'Pixeltype.ttf', 50)
 game_active = True
+start_time = 0
 
 sky_surface = pygame.image.load(graphics_path+'Sky.png').convert_alpha()
 ground_surface = pygame.image.load(graphics_path+'ground.png').convert_alpha()
 
-score_surface = test_font.render('My game', False, (64,64,64))
-score_rect = score_surface.get_rect(center = (WIDTH/2,50))
+# score_surface = test_font.render('My game', False, (64,64,64))
+# score_rect = score_surface.get_rect(center = (WIDTH/2,50))
 
 snail_surface = pygame.image.load(graphics_path+'snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom = (0,300))
@@ -45,18 +53,20 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 snail_rect.left = 800
+                start_time = int(pygame.time.get_ticks()/1000)
 
 
 
     if game_active:
         screen.blit(sky_surface,(0,0))
         screen.blit(ground_surface,(0,300))
-        pygame.draw.rect(screen, '#c0e8ec', score_rect)
-        pygame.draw.rect(screen, '#c0e8ec', score_rect,10)
+        # pygame.draw.rect(screen, '#c0e8ec', score_rect)
+        # pygame.draw.rect(screen, '#c0e8ec', score_rect,10)
 
-        #pygame.draw.line(screen, 'Gold', (0,0),(800,400),10)
+        # #pygame.draw.line(screen, 'Gold', (0,0),(800,400),10)
 
-        screen.blit(score_surface,score_rect)
+        # screen.blit(score_surface,score_rect)
+        display_score()
 
         snail_rect.x -= 5
         screen.blit(snail_surface, snail_rect)
