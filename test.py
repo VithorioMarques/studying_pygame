2f57860d4bdfafa838a77303fd1fa5aecd057ccd
Vithorio Marques <vithorio.marques@gmail.com>
2023-11-01 22:04:44 -0300

    Adding menu screen and score

--- test.py
+++ test.py
@@ -12,15 +12,34 @@ def display_score():
     score_surf = test_font.render(f'Score: {current_time}', False, (64,64,64))
     score_rect = score_surf.get_rect(center = (400,50))
     screen.blit(score_surf, score_rect)
+    return current_time
+
+def display_start_screen(score):
+    game_name_surf = test_font.render("Snail Jumper", False, (111,196,169))
+    game_name_rect = game_name_surf.get_rect(center = (400,80))
+    screen.blit(game_name_surf, game_name_rect)
+    if score == 0:
+        instructions_surf = test_font.render("Menu: PRESS SPACE TO START RUN", False, (111,196,169))
+        instructions_rect = instructions_surf.get_rect(center = (400,325))
+        screen.blit(instructions_surf, instructions_rect)
+        instructions_surf2 = test_font.render("Game: PRESS SPACE TO JUMP!!!", False, (111,196,169))
+        instructions_rect2 = instructions_surf2.get_rect(center = (400,375))
+        screen.blit(instructions_surf2, instructions_rect2)
+    else:
+        score_surf = test_font.render(f'Your score: {score}', False, (111,196,169))
+        score_rect = score_surf.get_rect(center = (400,325))
+        screen.blit(score_surf, score_rect)
+
 
 pygame.init()
 
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption('Beard Cutter')
+pygame.display.set_caption('Snail Jumper')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font(font_path+'Pixeltype.ttf', 50)
-game_active = True
+game_active = False
 start_time = 0
+score = 0
 
 sky_surface = pygame.image.load(graphics_path+'Sky.png').convert_alpha()
 ground_surface = pygame.image.load(graphics_path+'ground.png').convert_alpha()
@@ -34,6 +53,10 @@ snail_rect = snail_surface.get_rect(midbottom = (0,300))
 player_surf = pygame.image.load(graphics_path+'Player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80,300))
 player_gravity = 0
+
+player_stand = pygame.image.load(graphics_path + 'Player/player_stand.png')
+player_stand_scaled = pygame.transform.rotozoom(player_stand,0,2)
+player_stand_rect = player_stand_scaled.get_rect(center = (400,200))
 
 while True:
     for event in pygame.event.get():
@@ -66,7 +89,7 @@ while True:
         # #pygame.draw.line(screen, 'Gold', (0,0),(800,400),10)
 
         # screen.blit(score_surface,score_rect)
-        display_score()
+        score = display_score()
 
         snail_rect.x -= 5
         screen.blit(snail_surface, snail_rect)
@@ -94,7 +117,9 @@ while True:
         if snail_rect.colliderect(player_rect):
             game_active = False
     else:
-        screen.fill('Yellow')
+        screen.fill((94,128,162))
+        screen.blit(player_stand_scaled, player_stand_rect)
+        display_start_screen(score)
 
     pygame.display.update()
     clock.tick(60)
